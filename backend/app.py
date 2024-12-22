97a32335145117eb20594912fa9c97d92e92af70
will2dai4 <william.3dai@gmail.com>
2024-12-21 19:46:15 -0500

    more backend setup

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
-
-def hello_world():
+def home():
     return 'Hello, World'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
